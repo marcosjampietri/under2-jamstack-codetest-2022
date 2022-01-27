4c843fc84e9f4e7893116e2d6ac47670c1ba6785
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2022-01-27 17:47:33 +0000

    minfied JS, remove build script, ad dynamix import to components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
-import Head from "next/head";
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
 
-import { FeaturedBenefits } from "../components/sections/FeaturedBenefits";
-import { FeaturedPartners } from "../components/sections/FeaturedPartners/FeaturedPartners";
-import { GlobalFooter } from "../components/sections/GlobalFooter";
-import { GlobalNavigation } from "../components/sections/GlobalNavigation";
-import { HomeHero } from "../components/sections/HomeHero";
 
-import featuredPartnersJSON from "../content/featured-partners.json";
+const FeaturedBenefits = dynamic(() =>
+    import('../components/sections/FeaturedBenefits').then((mod) => mod.FeaturedBenefits)
+)
+
+// import { FeaturedBenefits } from '../components/sections/FeaturedBenefits'
+const FeaturedPartners = dynamic(() => import('../components/sections/FeaturedPartners/FeaturedPartners').then((mod) => mod.FeaturedPartners))
+const GlobalFooter = dynamic(() => import('../components/sections/GlobalFooter').then((mod) => mod.GlobalFooter))
+const GlobalNavigation = dynamic(() => import('../components/sections/GlobalNavigation').then((mod) => mod.GlobalNavigation))
+const HomeHero = dynamic(() => import('../components/sections/HomeHero').then((mod) => mod.HomeHero))
+
+import featuredPartnersJSON from "../content/featured-partners.json"
+
 
 export function getStaticProps() {
     return {
@@ -18,13 +25,8 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>
-                    Mula | The reporting tools taking the fintech world by storm
-                </title>
-                <meta
-                    name="description"
-                    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…"
-                />
+                <title>Mula | The reporting tools taking the fintech world by storm</title>
+                <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
@@ -36,5 +38,5 @@ export default function Home() {
             </div>
             <GlobalFooter />
         </>
-    );
+    )
 }
